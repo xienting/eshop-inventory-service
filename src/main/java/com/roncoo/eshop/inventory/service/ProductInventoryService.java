480b693e4e45c7b87041670ec11e41fd54a46b0d
hexin <390003727@qq.com>
2021-02-02 20:43:31 +0800

    first commit

---
+++ src/main/java/com/roncoo/eshop/inventory/service/ProductInventoryService.java
@@ -0,0 +1,15 @@
+package com.roncoo.eshop.inventory.service;
+
+import com.roncoo.eshop.inventory.model.ProductInventory;
+
+public interface ProductInventoryService {
+	
+	public void add(ProductInventory productInventory);
+	
+	public void update(ProductInventory productInventory);
+	
+	public void delete(Long id);
+	
+	public ProductInventory findById(Long id);
+	
+}
---
+++ src/main/java/com/roncoo/eshop/inventory/service/impl/ProductInventoryServiceImpl.java
@@ -0,0 +1,45 @@
+package com.roncoo.eshop.inventory.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import com.alibaba.fastjson.JSONObject;
+import com.roncoo.eshop.inventory.mapper.ProductInventoryMapper;
+import com.roncoo.eshop.inventory.model.ProductInventory;
+import com.roncoo.eshop.inventory.service.ProductInventoryService;
+
+@Service
+public class ProductInventoryServiceImpl implements ProductInventoryService {
+
+	@Autowired
+	private ProductInventoryMapper productInventoryMapper;
+	@Autowired
+	private JedisPool jedisPool;
+	
+	public void add(ProductInventory productInventory) {
+		productInventoryMapper.add(productInventory);
+		Jedis jedis = jedisPool.getResource();
+		jedis.set("product_inventory_" + productInventory.getProductId(), JSONObject.toJSONString(productInventory));
+	}
+	
+	public void update(ProductInventory productInventory) {
+		productInventoryMapper.update(productInventory); 
+		Jedis jedis = jedisPool.getResource();
+		jedis.set("product_inventory_" + productInventory.getProductId(), JSONObject.toJSONString(productInventory));
+	}
+
+	public void delete(Long id) {
+		ProductInventory productInventory = findById(id);
+		productInventoryMapper.delete(id); 
+		Jedis jedis = jedisPool.getResource();
+		jedis.del("product_inventory_" + productInventory.getProductId());//用的是商品id，不是库存id
+	}
+
+	public ProductInventory findById(Long id) {
+		return productInventoryMapper.findById(id);
+	}
+
+}
